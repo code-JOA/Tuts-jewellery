baea96557e404b102ca8ac85543804e2c4058269
Joshua Owusu <joshbillion1@gmail.com>
2023-09-14 10:53:17 +0000

    Commit auto_update

--- tuts/components/Header.tsx
+++ tuts/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header>
-      <div className="flex">
+      <div className="flex px[360]">
         <div>Menu left</div>
         <div>Menu Center</div>
         <div>Menu Right</div>
