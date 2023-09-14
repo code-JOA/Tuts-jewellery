11b69c2fa187dd1f9ebf7e4fc4dd04c596c51b2c
Joshua Owusu <joshbillion1@gmail.com>
2023-09-14 13:18:54 +0000

    Commit auto_update

--- tuts/components/Header.tsx
+++ tuts/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="fixed top-0 left-0 w-full h-[88px] flex items-center z-10 hover:bg-white transition-all duration-[3s] ease-in-out group">
       <div className="flex flex-1 items-center justify-between px-[36px] group-hover:text-black">
         {/* Left Header */}
-        <div className="flex items-center gap">
+        <div className="flex items-center gap-[30px]">
           <button type="button" className="flex items-center gap-4">
             <Bars3Icon className="h-6 w-6" />
             <span>Menu</span>
