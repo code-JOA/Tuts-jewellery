19dc22bfb8615131d15c436e28a2fbf622dbc081
Joshua Owusu <joshbillion1@gmail.com>
2023-09-14 10:55:24 +0000

    Commit auto_update

--- tuts/app/layout.tsx
+++ tuts/app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Header from "@/components/Header";
+
 // import { Inter } from "next/font/google";
 
 // const inter = Inter({ subsets: ["latin"] });
