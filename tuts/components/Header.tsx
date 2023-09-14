1b155f198b6f7c69d761e3667411ea298fd27760
Joshua Owusu <joshbillion1@gmail.com>
2023-09-14 10:52:11 +0000

    Commit auto_update

--- tuts/components/Header.tsx
+++ tuts/components/Header.tsx
@@ -1,5 +1,13 @@
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className="flex">
+        <div>Menu left</div>
+        <div>Menu Center</div>
+        <div>Menu Right</div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
