6c55c0bcda0438e8e8b9f16c7182dd10a11eec5e
Joshua Owusu <joshbillion1@gmail.com>
2023-09-14 11:38:18 +0000

    Commit auto_update

--- tuts/components/Header.tsx
+++ tuts/components/Header.tsx
@@ -1,11 +1,13 @@
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-[88px] flex items-center z-10 hover:bg-white transition-all duration-[3s] ease-in-out group">
-      <div className="flex flex-1 items-center justify-between px-[36px]">
+      <div className="flex flex-1 items-center justify-between px-[36px] group-hover:text-black">
         {/* Left Header */}
         <div>Menu left</div>
+
         {/* Center Header */}
         <div>Menu Center</div>
+
         {/* Right Header */}
         <div>Menu Right</div>
       </div>
