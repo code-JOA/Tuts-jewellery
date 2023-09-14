bd9b5e23542ab3787e5c14b0140ac092b8f193a3
Joshua Owusu <joshbillion1@gmail.com>
2023-09-14 10:33:47 +0000

    Commit auto_update

--- tuts/components/Header.tsx
+++ tuts/components/Header.tsx
@@ -1,7 +1,5 @@
-import React from "react";
+const Header = () => {
+  return <Header>Header</Header>;
+};
 
-type Props = {};
-
-export default function Header({}: Props) {
-  return <div>Header</div>;
-}
+export default Header;
